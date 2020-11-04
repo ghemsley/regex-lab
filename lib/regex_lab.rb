f7806307093f68505c4495f3982358627baa5001
ghemsley <ghemsley@protonmail.ch>
2020-11-04 11:54:50 -0600

    commit tree c14d4e7f276a47d74f1ee74c22df7711de9c3365

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  !word.match(/^[aeiou]/i).nil?
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/(\bun\w*ing\b)/im).flatten
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b([a-z][a-z][a-z][a-z][a-z])\b/im).flatten
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  !text.match(/^[A-Z]{1}.*[[:punct:]]{1}$/).nil?
 end
 
 def valid_phone_number?(phone)
-
+  !phone.match(/[(]*[\d]{3}[)]*[\-\ ]*[\d]{3}[\-\ ]*[\d]{4}(?![\d\-a-z])/i).nil?
 end
